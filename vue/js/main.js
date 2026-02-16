cc9650c32f0c18b677e3e342a8236fd937c5e28b
gr441_koaal <kulmanakovap@gmail.com>
2026-02-16 20:45:04 +0700

    add calculated property for title and sale

--- vue/js/main.js
+++ vue/js/main.js
@@ -1,31 +1,56 @@
 
+Vue.component('product', {
+    template: `
+   <div class="product">
+        <div class="product">
+        <div class="product-image">
+            <img :src="image" :alt="altText" />
+        </div>
+        <div class="product-info">
+            <h1>{{ title }}</h1>
+            <a :href="link"> More products like this</a>
+            <p v-if="inStock">In stock</p>
+            <p v-else :class="{ outOfStock: !inStock || inventory <= 0 }">Out of Stock</p>
+            <p>{{ sale }}</p>
+
+
+        </div>
+   </div>
+ `
+})
+
 let app = new Vue({
     el: '#app',
     data: {
         product: "Socks",
+        brand: 'Vue Mastery',
         description: "A pair of warm, fuzzy socks",
-        image: "./assets/vmSocks-blue-onWhite.jpg",
+        selectedVariant: 0,
         altText: "A pair of socks",
         link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
         inventory:100,
-        onSale: true,
-        detalis: ['80% cotton', '20% polyester', 'Gender-neutral'],
+        onSale: false,
+        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
         variants: [
             {
                 variantId: 2234,
                 variantColor: 'green',
                 variantImage: "./assets/vmSocks-green-onWhite.jpg",
+                variantQuantity: 10,
+                // variantSale: 'sale',
             },
             {
                 variantId: 2235,
                 variantColor: 'blue',
                 variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+                variantQuantity: 0,
+                // variantSale: 'Not sale',
+
 
             }
         ],
         sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
         cart: 0,
-        inStock: true,
 
     },
     methods: {
@@ -35,10 +60,25 @@ let app = new Vue({
         deleteToCart() {
             this.cart -= 1
         },
-        updateProduct(variantImage) {
-            this.image = variantImage
-        },
+        updateProduct(index) {
+            this.selectedVariant = index;
+            console.log(index);
+        }
+    },
+        computed: {
+            title() {
+                return this.brand + ' ' + this.product;
+            },
+            image() {
+                return this.variants[this.selectedVariant].variantImage
+            },
+            inStock(){
+                return this.variants[this.selectedVariant].variantQuantity
+            },
+            sale() {
+                return this.brand + ' ' + this.product + ' ' + this.onSale;
+            }
 
-    }
+        }
 })
 
