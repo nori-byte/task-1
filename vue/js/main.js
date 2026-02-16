68f876b20c55fe8b4fc82ebafbbf75c1d4de3caf
gr441_koaal <kulmanakoval@gmail.com>
2026-02-16 15:46:37 +0700

    add the button for adding an item to the cart

--- vue/js/main.js
+++ vue/js/main.js
@@ -8,5 +8,29 @@ let app = new Vue({
         altText: "A pair of socks",
         link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
         inStock: true,
+        inventory:100,
+        onSale: true,
+        detalis: ['80% cotton', '20% polyester', 'Gender-neutral'],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: 'green',
+                variantImage: "./assets/vmSocks-green-onWhite.jpg",
+            },
+            {
+                variantId: 2235,
+                variantColor: 'blue',
+                variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+
+            }
+        ],
+        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
+        cart: 0,
+
+    },
+    methods: {
+        addToCart() {
+            this.cart +=1
+        }
     }
 })
